adbfec7ebfce1eb48fba9e6489d2a03af4af6cc0
Flavio Granero <maltempe@gmail.com>
2014-06-25 14:48:01 -0300

    [#73047924] subscriptions migration and model

---
+++ app/models/subscription.rb
@@ -0,0 +1,8 @@
+class Subscription < ActiveRecord::Base
+  validates :user_id, :stack_id, :presence => true
+  validates_uniqueness_of :stack_id, scope: :user_id, allow_blank: true,
+                                    if: Proc.new{|u| u.user_id.present?}
+  
+  belongs_to :user
+  belongs_to :stack
+end
---
+++ db/migrate/20140625165821_create_subscriptions.rb
@@ -0,0 +1,12 @@
+class CreateSubscriptions < ActiveRecord::Migration
+  def change
+    create_table :subscriptions, id: :uuid do |t|
+      t.uuid :user_id,  null: false
+      t.uuid :stack_id, null: false
+
+      t.timestamps
+    end
+    
+    add_index :subscriptions, [:user_id, :stack_id], :unique => true
+  end
+end

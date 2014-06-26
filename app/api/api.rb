e605aa0223139b6d8991d5922094b482d89d23af
Flavio Granero <maltempe@gmail.com>
2014-06-26 11:53:37 -0300

    quickfix for swagger ui base_path

--- app/api/api.rb
+++ app/api/api.rb
@@ -15,9 +15,16 @@ class API < Grape::API
   mount Hyper::Stacks
   mount Hyper::Subscriptions
 
+  base_path_proc = Proc.new do |r|
+    if Rails.env.development?
+      "http#{r.base_url}"
+    else
+      "http://#{r.host}"
+    end
+  end
   add_swagger_documentation mount_path: 'api_docs',
                             api_version: 'v1',
                             hide_documentation_path: true,
                             hide_format: true,
-                            base_path: Proc.new { |r| "http#{r.base_url}" }
+                            base_path: base_path_proc
 end

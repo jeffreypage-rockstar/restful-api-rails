b12c3ae102c9502cd4beded4fa854cb9aae72054
Flavio Granero <maltempe@gmail.com>
2014-07-02 10:12:30 -0300

    [#74261120] adding users location field

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :username, :facebook_token, :avatar_url,
+  attributes :id, :email, :username, :facebook_token, :avatar_url, :location,
              :unconfirmed_email, :confirmed, :auth
 
   def confirmed

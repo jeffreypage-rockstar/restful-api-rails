86f7e101eb50235e61c3d9ef5a98f26cf69cd875
Flavio Granero <maltempe@gmail.com>
2014-07-28 14:57:43 -0300

    [#75733374] fixing server load

--- app/serializers/card_serializer.rb
+++ app/serializers/card_serializer.rb
@@ -1,3 +1,6 @@
+require_relative "card_image_serializer"
+require_relative "user_short_serializer"
+
 class CardSerializer < ActiveModel::Serializer
   attributes :id, :name, :user_id, :stack_id, :score, :flags_count, :my_vote,
              :comments_count, :created_at

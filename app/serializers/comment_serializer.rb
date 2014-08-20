61959ee86f20564f596d8f2679b05cfe6a36108f
Flavio Granero <maltempe@gmail.com>
2014-08-20 16:44:36 -0300

    [#77289116] returning flagged_by_me value for cards and comments json

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,9 +1,13 @@
 class CommentSerializer < ActiveModel::Serializer
   attributes :id, :body, :user_id, :username, :card_id, :score, :flags_count,
-             :my_vote, :created_at, :replying_id, :mentions
+             :my_vote, :created_at, :replying_id, :mentions, :flagged_by_me
 
   def my_vote
     VoteCardSerializer.new object.votes.where(user_id: current_user.id).first
+  end
+
+  def flagged_by_me
+    object.flags.exists? user_id: current_user.id
   end
 
   def username

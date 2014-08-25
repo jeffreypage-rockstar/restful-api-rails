2cc24f2bce43724240a8e821d103684dc0d64c37
Flavio Granero <maltempe@gmail.com>
2014-08-25 19:00:30 -0300

    comment serializer fix

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,3 +1,5 @@
+require_relative "user_short_serializer"
+
 class CommentSerializer < ActiveModel::Serializer
   attributes :id, :body, :user_id, :card_id, :score, :flags_count,
              :my_vote, :created_at, :replying_id, :mentions, :flagged_by_me

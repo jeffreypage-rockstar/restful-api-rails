724268cced631f2cc78e71072935013340aa9982
Flavio Granero <maltempe@gmail.com>
2014-09-01 14:16:41 -0300

    [#77803986] adding /available-usernames endpoint

--- app/api/hyper/usernames.rb
+++ app/api/hyper/usernames.rb
@@ -14,5 +14,23 @@ module Hyper
         end
       end
     end
+
+    resource "available-usernames" do
+      # GET /available-usernames/:username
+      desc "Returns a status 200 if username is avaliable, 404 if taken"
+      params do
+        requires :username, type: String, desc: "A username."
+      end
+      route_param :username do
+        get do
+          authenticate!
+          if User.find_by(username: params[:username])
+            raise ActiveRecord::RecordNotFound
+          else
+            true
+          end
+        end
+      end
+    end
   end
 end
--- spec/api/hyper/usernames_spec.rb
+++ spec/api/hyper/usernames_spec.rb
@@ -25,4 +25,24 @@ describe Hyper::Usernames do
       expect(response.status).to eql 404
     end
   end
+
+  # ======== CHECKING AN AVAILABLE USERNAME ==================
+  describe "GET /api/available-usernames/:username" do
+    it "requires authentication" do
+      get "/api/available-usernames/#{user.username}"
+      expect(response.status).to eql 401 # authentication
+    end
+
+    it "returns ok for an inexistent username" do
+      http_login device.id, device.access_token
+      get "/api/available-usernames/newusername", nil, @env
+      expect(response.status).to eql 200
+    end
+
+    it "returns not found for an existent username" do
+      http_login device.id, device.access_token
+      get "/api/available-usernames/#{user.username}", nil, @env
+      expect(response.status).to eql 404
+    end
+  end
 end

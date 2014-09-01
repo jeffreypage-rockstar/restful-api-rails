d6f6030dae26a79e917b2fb8040303248a8be04d
Flavio Granero <maltempe@gmail.com>
2014-09-01 17:08:40 -0300

    [#77803986] removing /usernames, making /available-usernames public

--- app/api/hyper/usernames.rb
+++ app/api/hyper/usernames.rb
@@ -1,20 +1,6 @@
 module Hyper
   # api to search for existent usernames
   class Usernames < Base
-    resource :usernames do
-      # GET /usernames/:username
-      desc "Returns a status 200 (found) or 404 (not found) given a username"
-      params do
-        requires :username, type: String, desc: "A username."
-      end
-      route_param :username do
-        get nil, serializer: UserShortSerializer do
-          authenticate!
-          User.find_by!(username: params[:username])
-        end
-      end
-    end
-
     resource "available-usernames" do
       # GET /available-usernames/:username
       desc "Returns a status 200 if username is avaliable, 404 if taken"
@@ -23,7 +9,6 @@ module Hyper
       end
       route_param :username do
         get do
-          authenticate!
           if User.find_by(username: params[:username])
             raise ActiveRecord::RecordNotFound
           else

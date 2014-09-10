9484d889804bba45a90b16953f2ecac5ed46a569
Andres <spike886@gmail.com>
2014-09-10 16:35:09 -0300

    add the forgotten id to notifications

--- app/serializers/notification_serializer.rb
+++ app/serializers/notification_serializer.rb
@@ -1,4 +1,4 @@
 class NotificationSerializer < ActiveModel::Serializer
   attributes :caption, :subject_id, :subject_type, :senders, :sent_at,
-             :read?, :seen?, :action, :image_url
+             :read?, :seen?, :action, :image_url, :id
 end

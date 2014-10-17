56e2cf82e6b8c9369d127d1b8b90a73470a2910b
Flavio Granero <maltempe@gmail.com>
2014-10-17 19:06:47 -0300

    [#fix] removing debug log

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,6 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_out_path_for(resource)
-    logger.info ">>>>#{resource.inspect}"
     if resource == :user
       home_path
     else

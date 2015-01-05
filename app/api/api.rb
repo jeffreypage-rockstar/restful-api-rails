a43fd6dadd2c7ce01fb9499ff6e6557d06162592
Flavio Granero <maltempe@gmail.com>
2015-01-05 20:09:34 -0200

    [#fix] adding request time benchmark to api logs

--- app/api/api.rb
+++ app/api/api.rb
@@ -9,9 +9,22 @@ class API < Grape::API
     header["Access-Control-Allow-Origin"] = "*"
     header["Access-Control-Request-Method"] = "*"
     unless Rails.env.test?
-      API.logger.info [request.env["REQUEST_METHOD"],
-                       request.env["REQUEST_PATH"]]
-      API.logger.info request.body.read
+      @log_start_t = Time.current
+      method = request.env["REQUEST_METHOD"]
+      path = request.env["REQUEST_PATH"]
+      API.logger.info ""
+      API.logger.info "Started #{method} \"#{path}\" at #{@log_start_t}"
+      API.logger.info "   Parameters: #{params.to_hash.except("route_info")}"
+    end
+  end
+
+  after do
+    unless Rails.env.test?
+      @log_end_t = Time.current
+      total_runtime = ((@log_end_t -  @log_start_t) * 1000).round(1)
+      db_runtime = (ActiveRecord::RuntimeRegistry.sql_runtime || 0).round(1)
+      API.logger.info "Completed in #{total_runtime}ms "\
+                      "(ActiveRecord: #{db_runtime}ms)"
     end
   end
 

211fb3b8f20a97920e4342bf417f735e916fdea4
Flavio Granero <maltempe@gmail.com>
2015-01-06 16:33:45 -0200

    new_relic manual start on server

--- server.rb
+++ server.rb
@@ -2,20 +2,19 @@ require File.expand_path("../config/api_application", __FILE__)
 require "app/api/api"
 require "goliath"
 require "em-synchrony/activerecord"
-require 'new_relic/rack/agent_hooks' 
-require 'new_relic/rack/browser_monitoring'
+require 'newrelic_rpm'
 
 API_SERVER = Rack::Builder.new do
   use Rack::Attack
   use Rack::Static, urls: %w(/docs/), root: "public/api", index: "index.html"
   use ActiveRecord::ConnectionAdapters::ConnectionManagement
-  use NewRelic::Rack::AgentHooks 
-  use NewRelic::Rack::BrowserMonitoring
 
   map "/" do
     run API
   end
 end
+
+NewRelic::Agent.manual_start({:env => Grape.application.config.env.to_s})
 
 # goliath server instance
 class Server < Goliath::API
